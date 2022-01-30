6a268390c154196edc14c93c54fd3e46bb198b17
unknown <oswasoto@espol.edu.ec>
2022-01-30 18:30:06 -0500

    Vista Reporte

--- src/main/java/com/mycompany/mavenproject1/VistaReporteController.java
+++ src/main/java/com/mycompany/mavenproject1/VistaReporteController.java
@@ -13,11 +13,15 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
@@ -35,42 +39,61 @@ public class VistaReporteController implements Initializable {
     @FXML
     private TextField txtMaterial;
     @FXML
-    private TableView<?> tbvTabla;
-
+    private TableView<CraterSensadoData> tbvTabla;
+    
+    private ObservableList<CraterSensadoData> craters;
+    @FXML
+    private TableColumn<CraterSensadoData, LocalDateTime> columnaFecha;
+    @FXML
+    private TableColumn<CraterSensadoData, String> columnaNombre;
+    @FXML
+    private TableColumn<CraterSensadoData, List<String>> columnaMinerales;
     /**
      * Initializes the controller class.
      */
-    
+    private ObservableList<CraterSensadoData> cratersFecha;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-       
+        try {
+            // TODO
+            tbvTabla.setItems((ObservableList<CraterSensadoData>) CraterSensadoData.leerCratersSensados());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+       /*
+        Comparator<CraterSensadoData> cmp = ((CraterSensadoData csd1, CraterSensadoData csd2)->{
+            int n = csd1.getNombre().compareTo(csd2.getNombre());
+            if(n == 0){
+                if(csd1.getFecha().isBefore(csd2.getFecha())) return -1;
+                if(csd1.getFecha().isAfter(csd2.getFecha())) return 1;
+                return 0;
+            }
+            return n;
+            
+        });
         
+        cratersFecha.sort(cmp);
+        columnaFecha.setCellValueFactory(new PropertyValueFactory<CraterSensadoData,LocalDateTime>("fecha"));
+        columnaNombre.setCellValueFactory(new PropertyValueFactory<CraterSensadoData,String>("nombre"));
+        columnaMinerales.setCellValueFactory(new PropertyValueFactory<CraterSensadoData,List<String>>("minerales"));
+       
+        craters = FXCollections.observableArrayList(cratersFecha);
+        tbvTabla.setItems(craters);
+        */
         
     }    
 
     @FXML
     private void filtrarFecha(KeyEvent event) {
-        Comparator<CraterSensadoData> cmp = ((CraterSensadoData csd1, CraterSensadoData csd2)->{
-            if(csd1.getFecha().isBefore(csd2.getFecha()))return -1;
-            if(csd1.getFecha().isBefore(csd2.getFecha())) return 1;
-            return 0;
-            
-        });
         
         if(txtFechaInicio.getText()!=null && txtFechaFin.getText() != null){
-            List<CraterSensadoData> craterFechaInicioFin = new ArrayList<>();
+            
             if(event.getCode() == KeyCode.ENTER){
                 try{
                     for(CraterSensadoData csd : CraterSensadoData.leerCratersSensados()){
-                        if(csd.fecha.isBefore(LocalDateTime.parse(txtFechaInicio.getText()))|| csd.fecha.isAfter(LocalDateTime.parse(txtFechaInicio.getText()))) craterFechaInicioFin.add(csd);
-                    
+                        if(csd.fecha.isBefore(LocalDateTime.parse(txtFechaInicio.getText().trim()))&& csd.fecha.isAfter(LocalDateTime.parse(txtFechaInicio.getText().trim()))) cratersFecha.add(csd);
+                          
                     }
-                    craterFechaInicioFin.sort(cmp);
-                    for(CraterSensadoData csdo:craterFechaInicioFin){
-                        
-                    }
-                    
                 }catch(IOException ex){
                     System.out.println(ex.getMessage());
                 }
